9198a503885c2d937aa5cf5368b697e448820b53
SmartArrow008 <59789213+SmartArrow008@users.noreply.github.com>
2020-03-15 15:50:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var gameState = 0;
+var playerCount;
+var database;
+var form,player,game;
+var allplayers;
+var distance = 0;
+var car1,car2,car3,car4,cars;
+function setup(){
+    database = firebase.database();
+    console.log(database);
+    createCanvas(displayWidth-20,displayHeight-30);
+    game = new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+   background("white");
+   if(playerCount===4){
+       game.update(1);
+   }
+   if(gameState===1){
+       clear();
+       game.play();
+   }
+
+   
+}
+
